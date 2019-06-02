395741bf1c42971f42401e34bcaf1a8d6a1b09f7
Quentin Tran <quentin.tran@telecom-sudparis.eu>
2019-06-02 20:11:58 +0200

    Début gestion des collisions

--- SaveTheGoozesSolution/SaveTheGoozesLib/WorldContactListener.cpp
+++ SaveTheGoozesSolution/SaveTheGoozesLib/WorldContactListener.cpp
@@ -1,10 +1,20 @@
 #include "pch.h"
 #include "WorldContactListener.h"
 #include <iostream>
-
+#include "Player.h"
+#include "Ennemy.h"
 
 void WorldContactListener::BeginContact(b2Contact* contact) {
 	std::cout << "On se rentre dedans !" << std::endl;
+	auto bodyUserDataA = contact->GetFixtureA()->GetBody()->GetUserData();
+	auto bodyuserDataB = contact->GetFixtureB()->GetBody()->GetUserData();
+	if (static_cast<Player*>(bodyUserDataA)) {
+		auto player = static_cast<Player*>(bodyUserDataA);
+		if (static_cast<Ennemy*>(bodyuserDataB)) {
+			auto enemy = static_cast<Ennemy*>(bodyuserDataB);
+			std::cout << "Le jour carresse un renard" << std::endl;
+		}
+	}
 }
 
 void WorldContactListener::EndContact(b2Contact* contact) {
